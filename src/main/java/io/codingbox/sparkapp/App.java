35a0df58f40a34d5f562587687aa8561eca9ee62
Valerii Iatsko <dwr@codingbox.io>
2016-08-29 21:13:31 +0200

    Added freemaker initialization

--- src/main/java/io/codingbox/sparkapp/App.java
+++ src/main/java/io/codingbox/sparkapp/App.java
@@ -3,9 +3,12 @@ package io.codingbox.sparkapp;
 import com.beust.jcommander.JCommander;
 import java.util.logging.Logger;
 
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.template.freemarker.FreeMarkerEngine;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
@@ -27,6 +30,11 @@ public class App
         logger.finest("Options.servicePort = " + options.servicePort);
 
         port(options.servicePort);
+
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
+        Configuration freeMarkerConfiguration = new Configuration();
+        freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(App.class, "/"));
+        freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
 
         get("/hello", (req, res) -> "Hello World");
 
