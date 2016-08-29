404c525d0137698e0cf11d0b5e0b11b8f760780a
Valerii Iatsko <dwr@codingbox.io>
2016-08-29 20:54:16 +0200

    Initial commit

---
+++ src/main/java/io/codingbox/react-redux-spark-boilerplate/App.java
@@ -0,0 +1,13 @@
+package io.codingbox.react-redux-spark-boilerplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
