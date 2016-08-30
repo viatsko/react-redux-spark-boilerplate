e3abdb0ec423be698a7949225e3bf8b551347de6
Valerii Iatsko <dwr@codingbox.io>
2016-08-30 20:23:29 +0200

    frontend/src/index.js: eslint fix

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
 import { App } from './containers/App';
 
