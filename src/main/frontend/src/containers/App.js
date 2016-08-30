1df1d9ab756e57ae639785863eeec7558e34fd8d
Valerii Iatsko <dwr@codingbox.io>
2016-08-30 07:40:07 +0200

    App cannot be stateless before react-hot-loader 3

--- src/main/frontend/src/containers/App.js
+++ src/main/frontend/src/containers/App.js
@@ -3,9 +3,14 @@ import React from 'react';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 
-export const App = () =>
-  <div>
-    <Header />
-    <div>Spark React Redux Boilerplate</div>
-    <Footer />
-  </div>;
+class App extends React.Component {
+  render() {
+    return <div>
+      <Header />
+      <div>Spark React Redux Boilerplate</div>
+      <Footer />
+    </div>;
+  }
+}
+
+export { App };
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
 import { App } from './containers/App';
 
+const store = createStore(
+  combineReducers({
+    routing: routerReducer,
+  })
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
